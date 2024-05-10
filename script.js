0f47ea016425207c5fd5ccb9722e4f1fc40cb776
ReD <a7exandr1@gmail.com>
2024-05-10 22:14:57 +0300

    implement the logic to get the computer choice

---
+++ script.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+    let random = Math.random() * 100;
+    let computerChoice = "";
+    if (random < 33) {
+        computerChoice = "rock";
+    } else if (random > 33 && random < 66) {
+        computerChoice = "paper"
+    } else {
+        computerChoice = "scissors";
+    }
+    return computerChoice;
+}
+
+let computer = getComputerChoice();
+console.log(computer);
+
+
+
